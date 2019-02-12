48316c1b8106b8a53911074ec8ed6d246b81a55c
ngando <yrr@live.fr>
2019-02-12 22:32:59 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { FrameComponent } from './frame/frame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+    FrameComponent
   ],
   imports: [
     BrowserModule,
